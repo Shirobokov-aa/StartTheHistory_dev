883af1abdba75c8ddc803c20c8afedfa452d151e
Shirobokov-aa <136621836+Shirobokov-aa@users.noreply.github.com>
2024-07-09 18:23:10 +0300

    header_adaptiv

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
 import './_components.js';
+
+// Маска для номера телефона 
+// document.addEventListener('DOMContentLoaded', function () {
+//     var input = document.getElementById('phone_number');
+//     var im = new Inputmask("+7 (999) 999-99-99"); // Маска для российского номера телефона
+//     im.mask(input);
+//   });
+
+// Для HEAD подключение
+//   <!-- Маска из CDN -->
+//   <script src="https://cdnjs.cloudflare.com/ajax/libs/inputmask/5.0.7/inputmask.min.js"></script>
